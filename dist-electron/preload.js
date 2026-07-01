b5cb0d00234d4fb51a3e8f6a71cf00778085656d
Vaibhav <vaibhav2k1507@gmail.com>
2026-07-01 15:56:41 +0530

    chore: untrack build outputs and environment variables

--- dist-electron/preload.js
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-contextBridge.exposeInMainWorld("electronAPI", {
-    saveToken: (token) => ipcRenderer.invoke("token:set", token),
-    getToken: () => ipcRenderer.invoke("token:get"),
-    deleteToken: () => ipcRenderer.invoke("token:delete"),
-});
